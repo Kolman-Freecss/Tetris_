172a2cd124d01f5a19dbbf27a0170c943a02db81
Sergio Martinez Roman <smartinezr@carver-as.com>
2020-01-10 10:07:55 +0100

    P10.01

---
+++ src/main/java/org/ragna/config/TetrisModule.java
@@ -0,0 +1,18 @@
+package org.ragna.config;
+
+import com.google.inject.AbstractModule;
+
+public class TetrisModule extends AbstractModule
+{
+
+	/**
+	 * We'll add the binds that we'll want to use in case of dependency 
+	 * */
+	@Override
+	protected void configure()
+	{
+		
+		
+	}
+
+}
--- src/main/java/org/ragna/model/ShapeType.java
+++ src/main/java/org/ragna/model/ShapeType.java
@@ -1,8 +1,55 @@
 package org.ragna.model;
 
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+/**
+ * @author smartinezr
+ * Rotations = cases of movement for every Shape
+ * BoundingBox = max points that the Shape could fill
+ * */
 public enum ShapeType
 {
-
+	//Box
+	C(		Arrays.asList(Sets.newHashSet()),
+			new Rectangle(0, 0, 2, 2));
 	
+	private final List<Set<Point>> rotations;
+	
+	private final Rectangle boundingBox;
+	
+	ShapeType(final List<Set<Point>> rotations,final Rectangle boundingBox)
+	{
+		this.rotations = rotations;
+		this.boundingBox = boundingBox;
+	}
+
+	public Rectangle getBoundingBox()
+	{
+		return boundingBox;
+	}
+	
+	public List<Set<Point>> getRotations()
+	{
+		return rotations;
+	}
+	
+	public enum LocationRotation
+	{
+		LOCATION_0_0(new Point(0, 0));
+		
+		private final Point point;
+		
+		LocationRotation(final Point point)
+		{
+			this.point = point;
+		}
+		
+	}
 	
 }
